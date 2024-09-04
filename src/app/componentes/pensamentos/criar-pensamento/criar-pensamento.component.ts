00da3e59cb2d861647fe92b6d960df0a20e59c38
Gerardo Rodrigues <62658809+GerardoRodrigues@users.noreply.github.com>
2024-09-04 19:29:51 -0300

    Correção do id na hora de criação de pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -10,7 +10,6 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent{
   pensamento: Pensamento = {
-    id: '',
     conteudo: '',
     autoria: '',
     modelo: ''
