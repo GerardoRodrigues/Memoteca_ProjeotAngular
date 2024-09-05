35b0db50e7829f87073e5d5662072e9d674a5285
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-05 20:26:15 -0300

    Paginação na lista de pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../Pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -10,10 +11,19 @@ import { PensamentoService } from '../pensamento.service';
 export class ListarPensamentoComponent implements OnInit{
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos = true;
 
   constructor(private service: PensamentoService){}
 
   ngOnInit(): void {
       this.service.listar(this.paginaAtual).subscribe((pensamentos) => this.listaPensamentos = pensamentos);
+  }
+
+  carregarMaisPensamentos(){
+    if(!this.service.haMaisPensamento){
+      this.haMaisPensamentos = false;
+      return;
+    }
+    this.service.listar(++this.paginaAtual).subscribe((pensamentos) => this.listaPensamentos.push(...pensamentos));
   }
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './Pensamento';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { response } from 'express';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,18 @@ export class PensamentoService {
   private readonly apiUrl = 'http://localhost:3000/pensamentos';
 
   constructor(private http: HttpClient) { }
+  haMaisPensamento: boolean = true;
 
   listar(pagina: number): Observable<Pensamento[]>{
     const itensPorPagina = 6;
-    let params = new HttpParams().set("_page", pagina).set("_limit", itensPorPagina);
-
-    return this.http.get<Pensamento[]>(this.apiUrl, {params})
+    let params = new HttpParams().set("_page", pagina).set("_per_page", itensPorPagina);
+    return this.http.get<any>(this.apiUrl, {params}).pipe(map(response => {
+      if(response.next === null){
+        this.haMaisPensamento = false;
+        return response.data;
+      }
+      return response.data;
+    }));
   }
 
   criar(pensamento: Pensamento):Observable<Pensamento>{
